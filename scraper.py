b2e3a897d052e0754ce90ff9e9932970c4eafb1d
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-09-09 17:41:51 +0100

    add category and genre to data collection script

--- scraper.py
+++ scraper.py
@@ -5,17 +5,20 @@ from bs4 import BeautifulSoup
 from helpers import random_string_generator, results_url_compiler
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 class Scraper:
 
-    def __init__(self, base_url):
+    def __init__(self):
         self.unique_descriptions = set()
-        self.all_shows_pd = pd.DataFrame()
+        self.all_shows_df = pd.DataFrame()
 
     def source_loader(self, url, driver_path):
         s = Service(driver_path)
-        self.driver = webdriver.Chrome(service=s)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(service=s, options=chrome_options)
         self.driver.get(url)
         self.driver.implicitly_wait(4)
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
@@ -70,6 +73,12 @@ class Scraper:
         title = div['data-event-name']
         return title
 
+    def category_genre_collector(self, div):
+        category_and_genre = div.find('h4').getText()
+        category, genres = category_and_genre.split('(')
+        genres = genres.strip(')(').split(', ')
+        return category, genres
+
     def description_is_valid(self, description):
         if description not in self.unique_descriptions and description != None:
             self.unique_descriptions.add(description)
@@ -88,12 +97,23 @@ class Scraper:
             if self.description_is_valid(description):
                 url = self.show_url_collector(div)
                 title = self.title_collector(div)
-                show_data = {'title': title, 'description': description, 'url': url}
+                category, genres = self.category_genre_collector(div)
+                show_data = {
+                    'title': title, 
+                    'description': description, 
+                    'url': url,
+                    'category': category,
+                    'genres': genres,
+                }
                 all_shows_on_page.append(
                     show_data
                 )
 
         return all_shows_on_page
+
+    def dataframe_appending(self, page_data):
+        page_df = pd.DataFrame(page_data)
+        self.all_shows_df = pd.concat([self.all_shows_df, page_df], ignore_index=True)
 
     def quit_driver_window(self):
         self.driver.quit()
@@ -102,10 +122,14 @@ class Scraper:
 def main():
     scraper = Scraper()
     for i in range(0, 3581, 10):
+        print(f'{int(i / 10)} / 358')
         url = results_url_compiler(BASE_URL, i)
         soup = scraper.source_loader(url, DRIVER_PATH)
-        scraper.single_page_collector(soup)
         scraper.quit_driver_window()
+        page_data = scraper.single_page_collector(soup)
+        scraper.dataframe_appending(page_data)
+        print(scraper.all_shows_df)
+        
 
 
 if __name__ == '__main__':
